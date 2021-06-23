ef5c971e59627a5dca02de738b97612e60209a92
Elijah Lucian <elijahlucian@gmail.com>
2021-06-22 23:04:55 -0600

    0.10.3

--- src/lib/lewperz/forI.ts
+++ src/lib/lewperz/forI.ts
@@ -1,6 +1,6 @@
 export const forI = <T>(
   arr: T[],
-  fn: (item: T, i: number, u: number) => any
+  fn: (item: T, i: number, u: number) => void
 ) => {
   for (let i = 0; i < arr.length; i++) fn(arr[i], i, i / arr.length);
 };
